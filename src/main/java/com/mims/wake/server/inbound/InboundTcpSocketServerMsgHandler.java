bbd64500d4aa0e871fadcc7427a64b93663f0bb5
jungkeehyun <musol80@gmail.com>
2020-02-19 23:10:56 +0900

    [InboundServer] pushMsg -> ByteBuf

---
+++ src/main/java/com/mims/wake/server/inbound/InboundTcpSocketServerMsgHandler.java
@@ -0,0 +1,100 @@
+package com.mims.wake.server.inbound;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mims.wake.server.queue.InboundQueue;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * Inbound Server와 송신자간 연결된 채널에서 발생하는 이벤트 처리용 핸들러
+ */
+public class InboundTcpSocketServerMsgHandler extends SimpleChannelInboundHandler<ByteBuf> {
+
+    private static final Logger LOG = LoggerFactory.getLogger(InboundTcpSocketServerMsgHandler.class);
+
+    private final Map<String, InboundQueue> inboundQueues;		// Inbound Queue collection
+
+    /**
+     * constructor with a parameter
+     * @param inboundQueues Inbound Queue collection
+     */
+    public InboundTcpSocketServerMsgHandler(Map<String, InboundQueue> inboundQueues) {
+        this.inboundQueues = inboundQueues;
+    }
+
+    /**
+     * 클라이언트와 채널이 연결되어 사용 가능한 상태가 되었을 때 동작<br>
+     * -연결 정보 로깅
+     * @param ctx ChannelHandlerContext object
+     * @see io.netty.channel.ChannelInboundHandlerAdapter#channelActive(io.netty.channel.ChannelHandlerContext)
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        LOG.info("@@@ MSG @@@@ [InboundServerHandler] connected {}", ctx.channel());
+        
+        try {
+        	LOG.info("[{}] Welcom to [{}]", new Date(), InetAddress.getLocalHost().getHostAddress());
+        } catch (UnknownHostException e) {
+        	e.printStackTrace();
+        }
+        
+        ctx.fireChannelActive();
+    }
+
+    /**
+     * 클라이언트로부터 메시지 수신했을 때 동작<br>
+     * -Service ID에 해당하는 InboundQueue에 추가
+     * @param ctx ChannelHandlerContext object
+     * @param msg 수신된 메시지
+     * @see io.netty.channel.SimpleChannelInboundHandler#channelRead0(io.netty.channel.ChannelHandlerContext, java.lang.Object)
+     */
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) {
+        LOG.info("@@@ MSG @@@@@ [InboundServerHandler] received {} from {}", msg.toString(), ctx.channel());
+
+        // Service ID에 해당하는 Inbound Queue에 메시지 추가
+        /*
+        String serviceId = msg.getServiceId();
+        if (serviceId != null && inboundQueues.containsKey(serviceId)) {
+            inboundQueues.get(serviceId).enqueue(msg);
+        } else {
+            LOG.warn("@@@@@@@@@ [InboundServerHandler] invalid service id in message {}", msg);
+        }
+        */
+    }
+
+    /**
+     * 클라이언트와 채널이 해제되어 사용 불가능한 상태가 되었을 때 동작<br>
+     * -연결해제 정보 로깅
+     * @param ctx ChannelHandlerContext object
+     * @see io.netty.channel.ChannelInboundHandlerAdapter#channelInactive(io.netty.channel.ChannelHandlerContext)
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        LOG.info("@@@ MSG @@@@@ [InboundServerHandler] disconnected {}", ctx.channel());
+    }
+
+    /**
+     * 채널의 I/O 오퍼레이션 도중 예외가 발생했을 때 동작<br>
+     * -예외 정보 로깅<br>
+     * -채널 연결해제
+     * @param ctx ChannelHandlerContext object
+     * @param cause 발생한 예외
+     * @see io.netty.channel.ChannelInboundHandlerAdapter#exceptionCaught(io.netty.channel.ChannelHandlerContext, java.lang.Throwable)
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        LOG.error("@@@ MSG @@@@@ [InboundServerHandler] error " + ctx.channel() + ", it will be closed", cause);
+        ctx.close();
+    }
+
+}
