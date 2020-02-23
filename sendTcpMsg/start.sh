549b4149c00902fc9bf8446b77ce57d8ad2161a6
jungkeehyun <60054183+jungkeehyun@users.noreply.github.com>
2020-02-23 15:50:50 +0900

    Update start.sh

--- sendTcpMsg/start.sh
+++ sendTcpMsg/start.sh
@@ -4,6 +4,6 @@
 #/Volumes/Data\ Storage/WORKSPACE/sendWake/mvnw clean package
 
 #cd target
-java -jar target/sendWake-0.0.1-SNAPSHOT.jar --sendWake.Ip=127.0.0.1 --sendWake.Port=13100 --sendWake.Sid=A2R
-java -jar target/sendWake-0.0.1-SNAPSHOT.jar --sendWake.Ip=127.0.0.1 --sendWake.Port=13100 --sendWake.Sid=S2R
-java -jar target/sendWake-0.0.1-SNAPSHOT.jar --sendWake.Ip=127.0.0.1 --sendWake.Port=13100 --sendWake.Sid=S2E
+java -jar target/sendWake-0.0.2-SNAPSHOT.jar --sendWake.Ip=127.0.0.1 --sendWake.Port=13100 --sendWake.Sid=A2R
+java -jar target/sendWake-0.0.2-SNAPSHOT.jar --sendWake.Ip=127.0.0.1 --sendWake.Port=13100 --sendWake.Sid=S2R
+java -jar target/sendWake-0.0.2-SNAPSHOT.jar --sendWake.Ip=127.0.0.1 --sendWake.Port=13100 --sendWake.Sid=S2E
